90123cdb4383455a30dbf52101a02f66938fac1c
Benjamin SELLEM <besellem@student.42.fr>
2021-07-20 17:44:32 +0200

    init

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/20 13:46:21 by besellem          #+#    #+#             */
+/*   Updated: 2021/07/20 15:34:04 by besellem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+char	*to_upper(char *s)
+{
+	for (int i = 0; s[i]; ++i) {
+		s[i] = toupper(s[i]);
+	}
+	return (s);
+}
+
+int		main(int ac, char **av) {
+	if (1 == ac)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	
+	for (int i = 1; i < ac; ++i) {
+		std::cout << to_upper(av[i]);
+		if ((i + 1) < ac) {
+			std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
