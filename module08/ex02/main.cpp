8a188b8107f0f19602e9198ffc085dc7b7d6d6e4
Benjamin SELLEM <besellem@student.42.fr>
2021-08-31 18:33:02 +0200

    all done

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 17:22:02 by besellem          #+#    #+#             */
-/*   Updated: 2021/08/31 18:17:16 by besellem         ###   ########.fr       */
+/*   Updated: 2021/08/31 18:32:47 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,62 @@
 
 int	main(void)
 {
-	MutantStack<int>	mstack;
-
-	mstack.push(5);
-	mstack.push(17);
-	
-	std::cout << "top:  [" GREEN << mstack.top() << CLR_COLOR "]" << std::endl;
-	mstack.pop();
-	std::cout << "size: [" GREEN << mstack.size() << CLR_COLOR "]" << std::endl;
-
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...] mstack.push(0);
-
-	MutantStack<int>::iterator	it = mstack.begin();
-	MutantStack<int>::iterator	ite = mstack.end();
-
-	++it;
-	--it;
-	while (it != ite)
+	std::cout << GREEN "# MutantStack Tests:" CLR_COLOR << std::endl;
 	{
-		std::cout << *it << std::endl;
+		MutantStack<int>	mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+		
+		std::cout << "top:  [" GREEN << mstack.top() << CLR_COLOR "]" << std::endl;
+		mstack.pop();
+		std::cout << "size: [" GREEN << mstack.size() << CLR_COLOR "]" << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...] mstack.push(0);
+
+		MutantStack<int>::iterator	it = mstack.begin();
+		MutantStack<int>::iterator	ite = mstack.end();
+
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int>		s(mstack);
 	}
+
+	std::cout << GREEN "# List Tests:" CLR_COLOR << std::endl;
+	{
+		std::list<int>		lst;
+
+		lst.push_back(5);
+		lst.push_back(17);
 	
-	std::stack<int>		s(mstack);
+		std::cout << "top:  [" GREEN << lst.back() << CLR_COLOR "]" << std::endl;
+		lst.pop_back();
+		std::cout << "size: [" GREEN << lst.size() << CLR_COLOR "]" << std::endl;
+
+		lst.push_back(3);
+		lst.push_back(5);
+		lst.push_back(737);
+		//[...] lst.push_back(0);
+
+		std::list<int>::iterator	it = lst.begin();
+		std::list<int>::iterator	ite = lst.end();
+
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	
 	return 0;
 }
